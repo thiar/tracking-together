af414945d91c5a7e66adba569602a04571b91dfc
thiar <thiar.hasbiya@gmail.com>
2015-05-12 13:26:21 +0700

    fix bug smooth moving marker

--- server.js
+++ server.js
@@ -18,13 +18,7 @@ app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', 'layout')
 
-// var files = new Static.Server('./public');
 
-// function handler (request, response) {
-// 	request.on('end', function() {
-// 		files.serve(request, response);
-// 	}).resume();
-// }
 var user={}
 app.use(session({
   secret: 'keyboard cat',
@@ -47,7 +41,7 @@ app.get("/", function(req,res){
 io.sockets.on('connection', function (socket) {
 
 	socket.on('log',function(data){
-		console.log(data)
+		//console.log(data)
 	})
 	socket.on('send:location',function(data){
 		//console.log(data)
@@ -60,7 +54,23 @@ io.sockets.on('connection', function (socket) {
 		console.log(user)
 	})
 	socket.on('connection:close', function (data) {
-	
+		for(var i in user)
+		{
+			if(user[i].socketid==socket.id)
+			{
+				var dataR={}
+				dataR.id=user[i].id
+				delete user[i];
+				io.emit('connection:remove',dataR)
+			}
+		}
+	});
+	socket.on('connection:update', function (data) {
+		user[data.id] = data;
+		io.emit('connection:updatelocation',data)
+		
+	});
+	socket.on('disconnect', function (data) {
 		for(var i in user)
 		{
 			
@@ -70,38 +80,15 @@ io.sockets.on('connection', function (socket) {
 				dataR.id=user[i].id
 				delete user[i];
 				io.emit('connection:remove',dataR)
-				//console.log("connection close")
-			}
-		}
-		//console.log("real "+socket.id)
-		
-	});
-	socket.on('connection:update', function (data) {
-		user[data.id] = data;
-		io.emit('connection:updatelocation',data)
-		//console.log("update "+data.id)
-	});
-	socket.on('disconnect', function (data) {
-		for(var i in user)
-		{
-			//console.log("i= "+i+"socket= "+user[i].socketid)
-			if(user[i].socketid==socket.id)
-			{
-				var dataR={}
-				dataR.id=user[i].id
-				delete user[i];
-				io.emit('connection:remove',dataR)
 				console.log("disconnect")
 			}
 		}
-		//console.log("real "+socket.id)
+		
 	});
 
 });
 
-// start app on specified port
-// app.listen(port);
-// console.log('Your server goes on localhost:' + port);
+
 http.listen(port, function(){
   console.log('listening on *:'+port);
 });
